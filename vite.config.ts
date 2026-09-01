7ac2691ecc27310209686854d1d3e575297b07e9
Sergio Garcia <107434475+sergiogarciamon@users.noreply.github.com>
2026-08-31 21:42:19 -0400

    Change base path to lowercase in vite config
    page updated to lower case

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/QA-Edge-Case-Generator/',
+  base: '/qa-edgecase-generator/',
   plugins: [react()],
 })
